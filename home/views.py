22ce83fb75acd995d33cd79112c568cca785e790
Luksh <hotmale_luksh@hotmail.com>
2022-05-06 09:25:32 +0545

    Hello

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
@@ -25,5 +26,6 @@ def contact(request):
         desc = request.POST.get('desc')
         contact = Contact(name = name, email = email, phone = phone, desc = desc, date = datetime.today())
         contact.save()
+        messages.success(request, 'Your message has been received!')
 
     return render(request, 'contact.html')
